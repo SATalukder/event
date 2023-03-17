e0570f9ffa685b089e49f812c29ce67c1131ac5f
SATalukder <shajibahmad152@gmail.com>
2023-03-17 20:39:32 +0600

    use event handler

--- js/event.js
+++ js/event.js
@@ -1,1 +1,28 @@
 console.log('This is separate js file');
+
+//option -2: add onclick function
+
+function makeYellow() {
+
+    // console.log(11);
+    document.body.style.backgroundColor = 'yellow';
+
+}
+
+//option -3: add onclick function
+const makeBlueButton = document.getElementById('make-blue');
+
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+//option 3: another way
+
+const makePurpleButton = document.getElementById('make-purple');
+makePurpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
+
+
